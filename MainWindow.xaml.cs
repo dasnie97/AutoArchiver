2b56f51aa5e41d57ce155d4b94cb2f14b2ee7fb5
Damian Śnieda <damiansnieda97@gmail.com>
2023-12-23 12:25:07 +0100

    Removed hard-coded pathTextBox and added dynamic creation of it together with buttons for edit and delete. Added events for edit/delete pf pathTextBox.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,8 +7,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
 
 namespace AutoArchiver
 {
@@ -34,36 +32,8 @@ namespace AutoArchiver
             _appSettingsManager = appSettingsManager;
         }
 
-        private void TextBlock_MouseEnter(object sender, MouseEventArgs e)
-        {
-            var background = "#FFE2DADA";
-            Color color = (Color)ColorConverter.ConvertFromString(background);
-            var brush = new SolidColorBrush(color);
-            textBox1.Background = brush;
-        }
-
-        private void textBox_MouseLeave(object sender, MouseEventArgs e)
-        {
-            textBox1.Background = null;
-        }
-
-        private void textBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            textBox1.IsReadOnly = false;
-        }
-
-        private void textBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            textBox1.IsReadOnly = true;
-        }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            var textBox = new TextBox();
-            textBox.Text = "";
-            
-            Test.Children.Add(textBox);
-
             var inputDir1 = _appSettingsManager.GetAppSettings().Config.Directories.Input;
 
             _appSettingsManager.GetAppSettings().Config.Directories.Input.Add("Dupa");
@@ -94,10 +64,71 @@ namespace AutoArchiver
 
         private void AddPathButton_Click(object sender, RoutedEventArgs e)
         {
-            TextBox textBox = new TextBox();
-            textBox.Text = "Text Box2";
+            string chosenPath = FileBrowserDialog.ChooseFolderUsingFolderExplorer();
+            if (chosenPath == string.Empty)
+            {
+                return;
+            }
 
-            Test.Children.Add(textBox) ;
+            Grid grid = new Grid();
+
+            ColumnDefinition pathColumn = new ColumnDefinition();
+            ColumnDefinition fileExplorerButtonColumn = new ColumnDefinition();
+            ColumnDefinition removePathButtonColumn = new ColumnDefinition();
+
+            pathColumn.Width = new GridLength(14, GridUnitType.Star);
+            fileExplorerButtonColumn.Width = new GridLength(1, GridUnitType.Star);
+            removePathButtonColumn.Width = new GridLength(1, GridUnitType.Star);
+
+            grid.ColumnDefinitions.Add(pathColumn);
+            grid.ColumnDefinitions.Add(fileExplorerButtonColumn);
+            grid.ColumnDefinitions.Add(removePathButtonColumn);
+
+            TextBox pathTextBox = new TextBox();
+            pathTextBox.Text = chosenPath;
+            pathTextBox.IsReadOnly = true;
+
+            Button fileExplorerButton = new Button();
+            fileExplorerButton.Width = 40;
+            fileExplorerButton.Height = 20;
+            fileExplorerButton.Content = "Edytuj";
+            fileExplorerButton.Click += FileExplorerButtonClick;
+
+            Button removePathButton = new Button();
+            removePathButton.Width = 40;
+            removePathButton.Height = 20;
+            removePathButton.Content = "Usun";
+            removePathButton.Click += RemovePathButtonClick;
+
+            grid.Children.Add(pathTextBox);
+            grid.Children.Add(fileExplorerButton);
+            grid.Children.Add(removePathButton);
+
+            Grid.SetColumn(pathTextBox, 0);
+            Grid.SetColumn(fileExplorerButton, 1);
+            Grid.SetColumn(removePathButton, 2);
+                        
+            Test.Children.Add(grid);
+        }
+
+        private void FileExplorerButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Grid grid = (Grid)button.Parent;
+            TextBox textBox = (TextBox)grid.Children[0];
+
+            string chosenPath = FileBrowserDialog.ChooseFolderUsingFolderExplorer();
+            if (chosenPath != string.Empty)
+            {
+                textBox.Text = chosenPath;
+            }
+        }
+
+        private void RemovePathButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Grid grid = (Grid)button.Parent;
+            Test.Children.Remove(grid) ;
         }
     }
 }
