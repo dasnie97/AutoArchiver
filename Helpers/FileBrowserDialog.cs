0bce57771d93692ae72cbfac9a55cc7c18e3df45
Damian Śnieda <damiansnieda97@gmail.com>
2023-12-23 12:22:20 +0100

    New helper for choosing folder using folder browser dialog.

---
+++ Helpers/FileBrowserDialog.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace AutoArchiver.Helpers;
+
+public class FileBrowserDialog
+{
+    public static string ChooseFolderUsingFolderExplorer()
+    {
+        FolderBrowserDialog openFileDlg = new FolderBrowserDialog();
+        var result = openFileDlg.ShowDialog();
+        if (result != DialogResult.Cancel)
+        {
+            return openFileDlg.SelectedPath;
+        }
+        else
+        {
+            return string.Empty;
+        }
+    }
+}
