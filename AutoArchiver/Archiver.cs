996ead398e5600db484118a797e3619ae6146035
Damian Śnieda <damiansnieda97@gmail.com>
2024-02-29 16:46:40 +0100

    Archiver dummy code

---
+++ AutoArchiver/Archiver.cs
@@ -0,0 +1,23 @@
+﻿using AutoArchiver.Shared;
+
+namespace AutoArchiver;
+
+public interface IArchiver
+{
+    Task<string> Archive();
+}
+
+public class Archiver : IArchiver
+{
+    public IAppSettingsManager _appSettingsManager { get; }
+
+    public Archiver(IAppSettingsManager appSettingsManager)
+    {
+        _appSettingsManager = appSettingsManager;
+    }
+
+    public Task<string> Archive()
+    {
+        return Task.Run(() => { return "Im a running code!"; });
+    }
+}
